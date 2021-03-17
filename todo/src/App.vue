e9d7dbbdf7121b597f6b5c182c07fd0e4000dd90
Armeet Jatyani <armeetjatyani@gmail.com>
2021-03-16 23:18:20 -0700

    Added, then removed tailwind

--- todo/src/App.vue
+++ todo/src/App.vue
@@ -1,26 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+	<div class="container">
+		<Header />
+	</div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+	import Header from "@/components/Header.vue";
 
-export default {
-  name: "App",
-  components: {
-    HelloWorld
-  }
-};
+	export default {
+		name: "App",
+		components: {
+			Header,
+		},
+	};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	@tailwind base;
+	@tailwind components;
+	@tailwind utilities;
+
+	#app {
+		--monospaced-font: Roboto Mono, monospace;
+		--main-font: Roboto, Arial, Helvetica, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+	}
 </style>
---
+++ todo/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+	<header>
+		<div class="pt-6 space-y-4">
+			<blockquote>
+				<p class="text-lg font-semibold">
+					“Tailwind CSS is the only framework that I've seen scale on
+					large teams. It’s easy to customize, adapts to any design,
+					and the build size is tiny.”
+				</p>
+			</blockquote>
+			<figcaption class="font-medium">
+				<div>
+					Sarah Dayan
+				</div>
+				<div>
+					Staff Engineer, Algolia
+				</div>
+			</figcaption>
+		</div>
+	</header>
+</template>
+
+<script>
+	export default {
+		name: "Header",
+	};
+</script>
+
+<style scoped></style>
